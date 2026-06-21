a56db8fea17cf0a9c97211b1b6ebea40eb1745e4
SAJJADSLK <56158654+SAJJADSLK@users.noreply.github.com>
2026-06-21 17:14:47 +0400

    Add files via upload

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -28,6 +28,7 @@ export default function Footer() {
               <li><Link href="/blog" className="hover:text-white transition-colors">Blog</Link></li>
               <li><Link href="/faq" className="hover:text-white transition-colors">FAQ</Link></li>
               <li><Link href="/about" className="hover:text-white transition-colors">About</Link></li>
+              <li><Link href="/contact" className="hover:text-white transition-colors">Contact</Link></li>
             </ul>
           </div>
 
